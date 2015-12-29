99d735bbef81d7898ff4e1251e6f49463d2ad985
Nicu Listana <niculistana@gmail.com>
2015-12-29 02:16:28 -0800

    Added user scaffold and sessions controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   resources :projects
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -7,6 +8,9 @@ Rails.application.routes.draw do
   get '/portfolio' => "projects#index"
   get '/blog' => "pages#blog"
   get '/about' => "pages#about"
+  get "login" => "sessions#new"
+  post "login" => "sessions#create"
+  delete 'logout' => 'sessions#destroy'
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151229064730) do
+ActiveRecord::Schema.define(version: 20151229093502) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "projectName"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 20151229064730) do
     t.string   "projectTag"
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
