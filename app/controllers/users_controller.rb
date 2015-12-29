03c5e1920c9f8f102ceafb066b2b65226332e4b8
Nicu Listana <niculistana@gmail.com>
2015-12-29 02:51:58 -0800

    Fixed new user link authorization, once again.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :require_user, only: [:index, :show, :new, :edit, :update, :destroy] 
+  before_action :require_user, only: [:index, :show, :edit, :create, :update, :destroy] 
 
   # GET /users
   # GET /users.json
